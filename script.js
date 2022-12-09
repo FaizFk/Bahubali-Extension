43bd08df4eb1b73ea18d4b3a734b1869f8b3ffb8
FaizFk <faiz262003@gmail.com>
2022-12-09 11:54:12 +0530

    initilized extension

---
+++ script.js
@@ -0,0 +1,10 @@
+handle = "faiz_titan";
+
+fetch(
+  "https://codeforces.com/api/user.status?handle=" + handle + "&from=1&count=10"
+)
+  .then((data) => data.json())
+  .then((cfData) => {
+    const p = document.getElementById("result");
+    p.innerHTML = cfData.result[0].verdict;
+  });
